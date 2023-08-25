9768dbad0eff146025b48b3c75dde93d3b8f772a
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-08-25 23:26:03 +0530

    Rock paper Scissor Header and Score section done

---
+++ JavaScript/RockPaperScissors/script.js
@@ -0,0 +1,13 @@
+let div = document.querySelector('.hellodiv')
+let main = document.querySelector('main')
+let button = document.querySelector('.addbtn')
+
+
+// button.addEventListener('click', () => {
+//     main.removeChild(div)
+//     let main2 = document.createElement('div')
+//     let h1 = document.createElement('h1')
+//     h1.innerText = "My name is Suman Bhandari"
+//     main2.appendChild(h1)
+//     main.prepend(main2)
+// })
