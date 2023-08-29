44591c3750a874c02a6b4123af2bccd6bda4924f
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-08-29 15:44:44 +0530

    JavaScript Logic Part Completed

--- JavaScript/RockPaperScissors/script.js
+++ JavaScript/RockPaperScissors/script.js
@@ -1,16 +1,30 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    
+    if (!localStorage.getItem('userScore')) {
+        localStorage.setItem('userScore', '0');
+    }
 
-// button.addEventListener('click', () => {
-//     main.removeChild(div)
-//     let main2 = document.createElement('div')
-//     let h1 = document.createElement('h1')
-//     h1.innerText = "My name is Suman Bhandari"
-//     main2.appendChild(h1)
-//     main.prepend(main2)
-// })
+    if (!localStorage.getItem('pcScore')) {
+        localStorage.setItem('pcScore', '0');
+  }
+  let userScoreInt = parseInt(localStorage.getItem('userScore'));
+let pcScoreInt = parseInt(localStorage.getItem('pcScore'));
 
-let ruleBtn = document.querySelector('.rule-btn')
+    
+   
+
+    
+    let userScore = document.querySelector('.user-score');
+    userScore.innerText = userScoreInt;
+
+    let pcScore = document.querySelector('.pc-score');
+    pcScore.innerText = pcScoreInt;
+
+    
+  let ruleBtn = document.querySelector('.rule-btn')
 let mainRuleContainer = document.querySelector('.main-rule-container') 
+
 
 function createRule() {
 let ruleConatiner1 = document.querySelector('.rule-container')
@@ -46,11 +60,261 @@ ruleList.appendChild(li4)
 ruleContainer.prepend(ruleHeader)
 ruleContainer.appendChild(ruleList)
  mainRuleContainer.prepend(ruleContainer)   
-
-console.log(ruleContainer)
     }
+  let crossBtn = document.querySelector('.cross-btn');  
+function removeRule() {
+    
+    let ruleContainer = document.querySelector('.rule-container')
+    mainRuleContainer.removeChild(ruleContainer)
+}
+
+crossBtn.addEventListener('click', () => {
+    removeRule()
+})
 }
 
 ruleBtn.addEventListener('click', () => {
+    
     createRule()
 })
+
+/**
+ * Game Play Section
+ */
+
+
+const handOptions = {
+    rock: {
+        Svg: "./assets/rock.svg",
+        Png:"./assets/rock.png"
+  },
+   paper: {
+        Svg: "./assets/paper.svg",
+        Png:"./assets/paper.png"
+  },
+   scissors: {
+        Svg: "./assets/scissors.svg",
+        Png:"./assets/scissors.png"
+  }
+}
+
+
+
+let rockBtn = document.querySelector('.rock')
+let paperBtn = document.querySelector('.paper')
+let scissorsBtn = document.querySelector('.scissors')
+
+function setUserScore() {
+    userScoreInt++;
+    localStorage.setItem('userScore', userScoreInt.toString());
+    let userScore = document.querySelector('.user-score');
+    userScore.innerText = userScoreInt;
+}
+function setPcScore() {
+    pcScoreInt++;
+    localStorage.setItem('pcScore', pcScoreInt.toString());
+    let pcScore = document.querySelector('.pc-score');
+    pcScore.innerText = pcScoreInt;
+}
+
+function setDecision(message, btnMessage, prop) {
+   let main  = document.querySelector('main')
+    let rockPaperScissors = document.querySelector('.rock-paper-scissors-container')
+    main.removeChild(rockPaperScissors)
+    
+    /**
+     * User Picked
+     */
+  let userPickedContainer = document.createElement('div');
+  userPickedContainer.classList="user-picked-container"
+  let userPickedImages = document.createElement('div')
+  userPickedImages.classList="user-picked-images"
+    let upicked = document.createElement('h3');
+    upicked.innerText="YOU PICKED"
+    let userPickSvg = document.createElement('img');
+    userPickSvg.src = handOptions[prop.userHand].Svg;
+    let userPickPng = document.createElement('img');
+  userPickPng.src = handOptions[prop.userHand].Png;
+  userPickedImages.appendChild(userPickSvg)
+  userPickedImages.appendChild(userPickPng)
+  userPickedContainer.prepend(upicked)
+  userPickedContainer.append(userPickedImages)
+    // console.log(userPickedContainer)
+    // console.log(userPickPng)
+  
+  
+  /**
+   * Msg And Play Again Area
+   */
+  let msgDiv = document.createElement('div');
+   msgDiv.classList="msg-container"
+  let msgHeader = document.createElement('h1');
+  let againstPc = document.createElement('h2');
+  let replyButton = document.createElement('button')
+  againstPc.innerText = "AGAINST PC"
+  if(message!="TIE UP")
+  {
+    msgHeader.innerText = `${ message }` 
+    
+    replyButton.classList="replay-btn"
+    replyButton.innerText = `${btnMessage}`
+    msgDiv.prepend(msgHeader)
+    msgDiv.appendChild(againstPc)
+    msgDiv.appendChild(replyButton)
+    
+    
+  }
+  else {
+    msgHeader.innerText = `${ message }` 
+    
+    replyButton.classList="replay-btn"
+    replyButton.innerText = `${btnMessage}`
+    msgDiv.prepend(msgHeader)
+     msgDiv.appendChild(replyButton)
+    
+  }
+ 
+  
+  
+    
+    /**
+     * Computer Picked
+     */
+  let pcPickedContainer = document.createElement('div');
+   pcPickedContainer.classList="pc-picked-container"
+  let pcPickedImages = document.createElement('div')
+  pcPickedImages.classList="pc-picked-images"
+    let cpicked = document.createElement('h3');
+    cpicked.innerText="PC PICKED"
+    let pcPickSvg = document.createElement('img');
+    pcPickSvg.src = handOptions[prop.cpHand].Svg;
+    let pcPickPng = document.createElement('img');
+  pcPickPng.src = handOptions[prop.cpHand].Png;
+  pcPickedImages.appendChild(pcPickSvg)
+  pcPickedImages.appendChild(pcPickPng)
+  pcPickedContainer.prepend(cpicked)
+  pcPickedContainer.appendChild(pcPickedImages)
+  // console.log(pcPickedContainer)
+   
+
+   
+  if (message === "YOU WIN") {
+    
+    let nextBtn = document.createElement('button');
+    nextBtn.classList = 'next-btn rule-btn ';
+    // nextBtn.classList='rule-btn'
+    nextBtn.innerText = "NEXT"
+    mainRuleContainer.appendChild(nextBtn)
+    // console.log(nextBtn)
+    userPickedContainer.setAttribute('id',"animation")
+    userPickedContainer.style.backgroundColor = "#2E9A2563"
+    
+  }
+  if (message == "YOU LOST") {
+  
+    
+    pcPickedContainer.setAttribute('id',"animation")
+    pcPickedContainer.style.backgroundColor="#2E9A2563"
+  }
+
+ 
+
+  let resultConatiner = document.createElement('div');
+  resultConatiner.classList = "result-container"
+  resultConatiner.style.display = 'flex'
+  resultConatiner.style.gap='2rem'
+  resultConatiner.prepend(userPickedContainer)
+  resultConatiner.appendChild(msgDiv)
+  resultConatiner.appendChild(pcPickedContainer)
+  main.prepend(resultConatiner)
+  console.log(resultConatiner)
+  function gameUI() {
+    let nextBtn = document.querySelector('.next-btn')
+      if (nextBtn) {
+       mainRuleContainer.removeChild(nextBtn)
+  
+    }
+      main.removeChild(resultConatiner)
+      
+        main.prepend(rockPaperScissors) 
+     }
+
+    replyButton.addEventListener('click', () => {
+        gameUI()
+    })
+}
+function referee(userHand, cpHand) {
+    if (userHand == "paper" && cpHand == "scissors") {
+      setPcScore()  
+    setDecision("YOU LOST","PLAY AGAIN",{userHand,cpHand});
+  }
+  if (userHand == "paper" && cpHand == "rock") {
+    setDecision("YOU WIN","PLAY AGAIN",{userHand,cpHand});
+    setUserScore();
+  }
+  if (userHand == "paper" && cpHand == "paper") {
+    setDecision("TIE UP","REPLAY",{userHand,cpHand});
+  }
+  if (userHand == "rock" && cpHand == "scissors") {
+    setDecision("YOU WIN","PLAY AGAIN",{userHand,cpHand});
+    setUserScore();
+  }
+  if (userHand == "rock" && cpHand == "paper") {
+    setDecision("YOU LOST","PLAY AGAIN",{userHand,cpHand});
+      setPcScore()
+  }
+  if (userHand == "rock" && cpHand == "rock") {
+    setDecision("TIE UP","REPLAY",{userHand,cpHand});
+  }
+  if (userHand == "scissors" && cpHand == "scissors") {
+    setDecision("TIE UP","REPLAY",{userHand,cpHand});
+  }
+  if (userHand == "scissors" && cpHand == "rock") {
+      setDecision("YOU LOST","PLAY AGAIN",{userHand,cpHand});
+      
+      setPcScore()
+  }
+  if (userHand == "scissors" && cpHand == "paper") {
+    setDecision("YOU WIN","PLAY AGAIN",{userHand,cpHand});
+    setUserScore();
+  }
+}
+
+function pickComputerHand(hand) {
+    
+    let hands = ['rock', 'paper', 'scissors'];
+    let cpHand = hands[Math.floor(Math.random() * hands.length)]
+    // console.log(`Computer choose ${cpHand}`)
+    // console.log(`User choose ${hand}`)
+    referee(hand,cpHand)
+}
+
+function pickUserHand(hand) {
+    pickComputerHand(hand)
+}
+
+
+rockBtn.addEventListener('click', () => {
+    pickUserHand("rock")
+})
+
+paperBtn.addEventListener('click', () => {
+    pickUserHand("paper")
+})
+scissorsBtn.addEventListener('click', () => {
+    pickUserHand("scissors")
+})
+});
+
+
+// button.addEventListener('click', () => {
+//     main.removeChild(div)
+//     let main2 = document.createElement('div')
+//     let h1 = document.createElement('h1')
+//     h1.innerText = "My name is Suman Bhandari"
+//     main2.appendChild(h1)
+//     main.prepend(main2)
+// })
+/**
+ * TO create a rule
+ */
