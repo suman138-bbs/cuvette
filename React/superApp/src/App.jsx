2519805a6ced39eb6459ee08d2e18cbd25c579d7
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-10-03 12:49:42 +0530

    signup page

--- React/superApp/src/App.jsx
+++ React/superApp/src/App.jsx
@@ -1,9 +1,12 @@
 import Regiter from "./pages/Register/register.component";
+import Dashbord from "./pages/Dashbord/dashbord.component";
+import { Route, Routes } from "react-router-dom";
 const App = () => {
   return (
-    <>
-      <Regiter />
-    </>
+    <Routes>
+      <Route path="/" element={<Regiter />} />
+      <Route path="/dashbord" element={<Dashbord />} />
+    </Routes>
   );
 };
 
--- React/superApp/src/components/signup/signup.component.jsx
+++ React/superApp/src/components/signup/signup.component.jsx
@@ -1,5 +1,63 @@
+import { useState } from "react";
 import style from "./signup.module.css";
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
+  const defaultError = {
+    errorName: "",
+    errorUsername: "",
+    errorEmail: "",
+    errorNumber: "",
+    errorAgree: "",
+  };
+
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    email: "",
+    number: "",
+    agree: "",
+  });
+
+  const [error, setError] = useState(defaultError);
+  const dataValidate = (data) => {
+    if (/\d/.test(data.name)) {
+      setError({
+        ...defaultError,
+        errorName: "Name should not contain numbers.",
+      });
+    } else if (data.name === "") {
+      setError({ ...defaultError, errorName: "Field is required" });
+    } else if (data.username === "") {
+      setError({ ...defaultError, errorUsername: "Field is required" });
+    } else if (data.email === "") {
+      setError({ ...defaultError, errorEmail: "Field is required" });
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(data.email)) {
+      setError({ ...defaultError, errorEmail: "Invalid Email" });
+    } else if (data.mobile === "") {
+      setError({ ...defaultError, errorNumber: "Field is required" });
+    } else if (data.number?.toString()?.length < 10) {
+      setError({ ...defaultError, errorNumber: "Invalid Number" });
+    } else if (data.agree !== true) {
+      setError({ ...defaultError, errorAgree: "Field is required" });
+    } else {
+      setError(defaultError);
+      const data = JSON.stringify(formData);
+      localStorage.setItem("formData", data);
+
+      console.log(JSON.parse(localStorage.getItem("formData")));
+      navigate("/dashbord");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dataValidate(formData);
+  };
+
+  const handleChange = (value) => {
+    setFormData({ ...value });
+  };
   return (
     <div className={style.signUpContainer}>
       <form>
@@ -8,20 +66,73 @@ const Signup = () => {
           <p>Create Your new account</p>
         </div>
         <div className={style.inputContainer}>
-          <input type="text" placeholder="Name" required />
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              required
+              onChange={(e) => {
+                handleChange({ ...formData, name: e.target.value });
+              }}
+            />
+            <p className={style.error}>{error.errorName}</p>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="UserName"
+              required
+              onChange={(e) => {
+                handleChange({ ...formData, username: e.target.value });
+              }}
+            />
+            <p className={style.error}>{error.errorUsername}</p>
+          </div>
 
-          <input type="text" placeholder="UserName" required />
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              onChange={(e) => {
+                handleChange({ ...formData, email: e.target.value });
+              }}
+            />
+            <p className={style.error}>{error.errorEmail}</p>
+          </div>
 
-          <input type="email" placeholder="Email" required />
-
-          <input type="mobile" placeholder="Mobile" required />
+          <div>
+            <input
+              type="text"
+              placeholder="Mobile"
+              required
+              onChange={(e) => {
+                handleChange({ ...formData, number: e.target.value });
+              }}
+            />
+            <p className={style.error}>{error.errorNumber}</p>
+          </div>
         </div>
         <div className={style.checkBox}>
-          <input type="checkbox" required />
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              handleChange({ ...formData, agree: !formData.agree });
+            }}
+          />
+
           <label htmlFor="">Share my registration data with Superapp</label>
+          <p className={style.error}>{error.errorAgree}</p>
         </div>
+
         <div className={style.termsAndCondition}>
-          <button>SIGN UP</button>
+          <button
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            SIGN UP
+          </button>
           <p>
             By clicking on Sign up. you agree to Superapp{" "}
             <span>Terms and Conditions of Use</span>
