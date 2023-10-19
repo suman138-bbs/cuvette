ffb20add373e45081899423c1fdd2c818fbfbd50
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-10-19 20:46:27 +0530

    Entertainment component

--- React/superApp/src/pages/entertainment/entertainment.component.jsx
+++ React/superApp/src/pages/entertainment/entertainment.component.jsx
@@ -2,49 +2,78 @@ import { useState, useEffect } from "react";
 import style from "./entertainment.module.css";
 import Rounded from "../../assets/Rounded.png";
 import { Link } from "react-router-dom";
-import axios from "axios";
+
 const Entertainment = () => {
   const [selectedItem, setSelectedItem] = useState([]);
+  const [genreData, setGenreData] = useState({});
 
   useEffect(() => {
-    setSelectedItem(JSON.parse(localStorage.getItem("selectedItem")));
-
-    const getData = async () => {
-      const url =
-        "https://unogs-unogs-v1.p.rapidapi.com/search/titles?order_by=date&type=movie";
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "4e57850dc2msh190ba06551ed657p1b4d91jsn8901d1ca67d8",
-          "X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com",
-        },
-      };
-
-      try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getData();
+    const storedItem = localStorage.getItem("selectedItem");
+    if (storedItem) {
+      setSelectedItem(JSON.parse(storedItem));
+    }
   }, []);
-  console.log(selectedItem);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiKey = "43dcdfb63ef043263b51235b65fbf1a8";
+      const genreDataObject = {};
+
+      for (const item of selectedItem) {
+        const genreId = item.id;
+        const data = await getData(apiKey, genreId);
+        genreDataObject[genreId] = data.results.slice(0, 4);
+      }
+
+      setGenreData(genreDataObject);
+    };
+
+    fetchData();
+  }, [selectedItem]);
+
+  const getData = async (apiKey, genreId) => {
+    try {
+      const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("An error occurred:", error);
+      return { results: [] };
+    }
+  };
+  console.log(genreData);
   return (
     <div className={style.entertainContainer}>
       <div className={style.headerContainer}>
         <p>Super app</p>
         <div>
           <Link to="/dashbord">
-            {" "}
-            <img src={Rounded} alt="" srcset="" />
+            <img src={Rounded} alt="" srcSet="" />
           </Link>
         </div>
       </div>
-      <div className={style.heroConatiner}>
+      <div className={style.heroContainer}>
         <p>Entertainment according to your choice</p>
+        <div>
+          {selectedItem.map((item) => (
+            <div key={item.id}>
+              <h3 className={style.nameHeader}>{item.name}</h3>
+              <div className={style.movieItemContainer}>
+                {genreData[item.id]?.map(({ original_title, poster_path }) => (
+                  <div>
+                    <p key={original_title}>{original_title}</p>
+                    <div>
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- React/superApp/src/utils/data.js
+++ React/superApp/src/utils/data.js
@@ -1,46 +1,46 @@
 export const data = [
   {
-    id: 1,
+    id: 28,
     name: "Action",
     imageName: "action.png",
   },
   {
-    id: 2,
+    id: 18,
     name: "Drama",
     imageName: "drama.png",
   },
   {
-    id: 3,
+    id: 10749,
     name: "Romance",
     imageName: "romance.png",
   },
   {
-    id: 4,
+    id: 53,
     name: "Thriller",
     imageName: "thriller.png",
   },
   {
-    id: 5,
+    id: 37,
     name: "Western",
     imageName: "western.png",
   },
   {
-    id: 6,
+    id: 27,
     name: "Horror",
     imageName: "horror.png",
   },
   {
-    id: 7,
+    id: 14,
     name: "Fantasy",
     imageName: "fantasy.png",
   },
   {
-    id: 8,
+    id: 10402,
     name: "Music",
     imageName: "music.png",
   },
   {
-    id: 9,
+    id: 878,
     name: "Fiction",
     imageName: "fiction.png",
   },
